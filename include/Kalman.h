22eb2e9ad3e8e648eb23e9dea0822a68c0c6a56e
Julio C. Villalta III <jvillalta@nvisia.com>
2015-06-14 22:28:31 -0500

    Updated Kalman.h to have comments

--- include/Kalman.h
+++ include/Kalman.h
@@ -1,6 +1,6 @@
-#pragma once
 #include "opencv2/opencv.hpp"
 #include <opencv/cv.h>
+
 using namespace cv;
 using namespace std;
 
@@ -9,11 +9,14 @@ using namespace std;
 class TKalmanFilter
 {
 public:
+	//constructor and destructor
+	TKalmanFilter(Point2f p,float dt=0.2,float Accel_noise_mag=0.5);
+	~TKalmanFilter();
+	
 	KalmanFilter* kalman;
 	double deltatime; 
 	Point2f LastResult;
-	TKalmanFilter(Point2f p,float dt=0.2,float Accel_noise_mag=0.5);
-	~TKalmanFilter();
+	
 	Point2f GetPrediction();
 	Point2f Update(Point2f p, bool DataCorrect);
 };
